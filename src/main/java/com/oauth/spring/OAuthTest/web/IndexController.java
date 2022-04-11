9a1ace22e525d61561c6b136ccb0ef73a5f82ea0
Sion Noh <shtldhs123@ajou.ac.kr>
2022-04-11 20:39:46 +0900

    웹에서 사용자 이름 보이게 설정, 로그인시 세션을 가짐

--- src/main/java/com/oauth/spring/OAuthTest/web/IndexController.java
+++ src/main/java/com/oauth/spring/OAuthTest/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.oauth.spring.OAuthTest.web;
 
+import com.oauth.spring.OAuthTest.config.auth.dto.SessionUser;
 import com.oauth.spring.OAuthTest.service.posts.PostsService;
 import com.oauth.spring.OAuthTest.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -8,15 +9,22 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
     public String index(Model model) {
         model.addAttribute("posts", postsService.findAllDesc());
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+        if (user != null) {
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
     }
 
