b69b5e471665c4bb71665fbbdbbf24adaf7cde6c
Sion Noh <shtldhs123@ajou.ac.kr>
2022-04-11 18:28:48 +0900

    게시물 수정/조회 api, 생성시간/수정시간 자동화(JPA Auditing)

--- src/test/java/com/oauth/spring/OAuthTest/domain/PostsRepositoryTest.java
+++ src/test/java/com/oauth/spring/OAuthTest/domain/PostsRepositoryTest.java
@@ -1,6 +1,7 @@
-package com.oauth.spring.OAuthTest.domain.posts;
+package com.oauth.spring.OAuthTest.domain;
 
-import org.aspectj.lang.annotation.After;
+import com.oauth.spring.OAuthTest.domain.posts.Posts;
+import com.oauth.spring.OAuthTest.domain.posts.PostsRepository;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -8,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -43,6 +45,29 @@ public class PostsRepositoryTest {
         Posts posts = postsList.get(0);
         assertThat(posts.getTitle()).isEqualTo(title);
         assertThat(posts.getContent()).isEqualTo(content);
-        
+
+    }
+
+    @Test
+    public void BaseTimeEntity_등록() {
+        //given
+        LocalDateTime now = LocalDateTime.of(2022, 4, 11, 0, 0, 0);
+        postsRepository.save(Posts.builder()
+                .title("title")
+                .content("content")
+                .author("author")
+                .build());
+
+        //when
+        List<Posts> postsList = postsRepository.findAll();
+
+        //then
+        Posts posts = postsList.get(0);
+
+        System.out.println(">>>>>>>> createDate=" + posts.getCreatedDate() +
+                ", modifiedDate=" + posts.getModifiedDate());
+        assertThat(posts.getCreatedDate()).isAfter(now);
+        assertThat(posts.getModifiedDate()).isAfter(now);
+
     }
 }
--- src/test/java/com/oauth/spring/OAuthTest/web/PostsApiControllerTest.java
+++ src/test/java/com/oauth/spring/OAuthTest/web/PostsApiControllerTest.java
@@ -3,6 +3,7 @@ package com.oauth.spring.OAuthTest.web;
 import com.oauth.spring.OAuthTest.domain.posts.Posts;
 import com.oauth.spring.OAuthTest.domain.posts.PostsRepository;
 import com.oauth.spring.OAuthTest.web.dto.PostsSaveRequestDto;
+import com.oauth.spring.OAuthTest.web.dto.PostsUpdateRequestDto;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -10,6 +11,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
@@ -61,6 +64,47 @@ public class PostsApiControllerTest {
         List<Posts> all = postsRepository.findAll();
         assertThat(all.get(0).getTitle()).isEqualTo(title);
         assertThat(all.get(0).getContent()).isEqualTo(content);
+
+    }
+
+    @Test
+    public void Posts_수정() throws Exception {
+        //given
+        Posts savedPost = postsRepository.save(Posts.builder()
+                .title("테스트 타이틀")
+                .content("테스트 컨텐트")
+                .author("테스트 author")
+                .build());
+
+        Long updateId = savedPost.getId();
+        String expectedTitle = "title2";
+        String expectedContent = "content2";
+
+        PostsUpdateRequestDto requestDto = PostsUpdateRequestDto.builder()
+                .title(expectedTitle)
+                .content(expectedContent)
+                .build();
+
+        String url = "http://localhost:" + port + "/api/v1/posts/" + updateId;
+
+        /*
+            HttpEntity
+            - Http 프로토콜을 이용하는 통신의 header와 body 관련 정보를 저장할 수 있도록 함
+            - 이를 상속받은 클래스: RequestEntity, ResponseEntity
+         */
+        HttpEntity<PostsUpdateRequestDto> requestEntity = new HttpEntity<>(requestDto);
+
+        //when
+        ResponseEntity<Long> responseEntity = restTemplate.exchange(
+                url, HttpMethod.PUT, requestEntity, Long.class);
+
+        //then
+        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(responseEntity.getBody()).isGreaterThan(0L);
+
+        List<Posts> all = postsRepository.findAll();
+        assertThat(all.get(0).getTitle()).isEqualTo(expectedTitle);
+        assertThat(all.get(0).getContent()).isEqualTo(expectedContent);
 
     }
 
