8ef801eff6354262f13c2714be8e7f57f2ad0e9d
Sion Noh <shtldhs123@ajou.ac.kr>
2022-04-11 20:32:36 +0900

    SessionUser Dto에 직렬화 기능 추가

--- src/main/java/com/oauth/spring/OAuthTest/config/auth/dto/SessionUser.java
+++ src/main/java/com/oauth/spring/OAuthTest/config/auth/dto/SessionUser.java
@@ -3,6 +3,8 @@ package com.oauth.spring.OAuthTest.config.auth.dto;
 import com.oauth.spring.OAuthTest.domain.user.User;
 import lombok.Getter;
 
+import java.io.Serializable;
+
 /*
 # User 클래스를 사용하면 안되고, 따로 만들어야 하는 이유
 -> User 클래스를 세션에 저장하려고 하면, convert from type [Object] to type [byte[]]가 불가능하다는 에러가 발생한다.
@@ -10,7 +12,7 @@ import lombok.Getter;
 그래서 직렬화 기능을 가진 세션 Dto를 하나 추가로 만드는 것이 큰 도움이 된다.
  */
 @Getter
-public class SessionUser {
+public class SessionUser implements Serializable {
 
     private String name;
     private String email;
