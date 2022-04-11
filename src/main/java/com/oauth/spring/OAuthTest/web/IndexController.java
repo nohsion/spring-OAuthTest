7c8d1f6e1b8f35a42f61d54713419eca5628744b
Sion Noh <shtldhs123@ajou.ac.kr>
2022-04-11 20:53:44 +0900

    어노테이션 기반으로 개선하기 @LoginUser

--- src/main/java/com/oauth/spring/OAuthTest/web/IndexController.java
+++ src/main/java/com/oauth/spring/OAuthTest/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.oauth.spring.OAuthTest.web;
 
+import com.oauth.spring.OAuthTest.config.auth.LoginUser;
 import com.oauth.spring.OAuthTest.config.auth.dto.SessionUser;
 import com.oauth.spring.OAuthTest.service.posts.PostsService;
 import com.oauth.spring.OAuthTest.web.dto.PostsResponseDto;
@@ -19,9 +20,9 @@ public class IndexController {
     private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+
         if (user != null) {
             model.addAttribute("userName", user.getName());
         }
