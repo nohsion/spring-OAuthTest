2718d4ade4c4754e9988d1d797590d3643eb20c5
Sion Noh <shtldhs123@ajou.ac.kr>
2022-04-11 16:40:20 +0900

    Initial commit

---
+++ src/main/java/com/oauth/spring/OAuthTest/OAuthTestApplication.java
@@ -0,0 +1,13 @@
+package com.oauth.spring.OAuthTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OAuthTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OAuthTestApplication.class, args);
+	}
+
+}
