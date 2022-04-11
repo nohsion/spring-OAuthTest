eec6b485de07dbf02856747f81e6d51ef1d6de30
Sion Noh <shtldhs123@ajou.ac.kr>
2022-04-11 17:00:12 +0900

    spring data JPA 테스트 코드

---
+++ src/main/java/com/oauth/spring/OAuthTest/domain/posts/PostsRepository.java
@@ -0,0 +1,6 @@
+package com.oauth.spring.OAuthTest.domain.posts;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostsRepository extends JpaRepository<Posts, Long> {
+}
